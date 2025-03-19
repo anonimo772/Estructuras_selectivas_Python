445c17a688bba5e83d5fc0c5eae78d767d00dd94
unknown <Administrador@BOGDFPCGMF4137.FormacionSena.RED>
2025-03-19 11:00:15 -0500

    Actividad_2.py

---
+++ Actividad_2.py
@@ -0,0 +1,24 @@
+estrato = int(input("Ingresa tu estrato: "))
+edad = int(input("Ingresa tu edad: "))
+precio_matricula = 2000000
+
+
+if edad > 18 and estrato == 1:
+    descuento = 0.20  # 20%
+    print("Tu descuento es del 20%")
+elif edad <= 18 and estrato == 1:
+    descuento = 0.15  # 15%
+    print("Tu descuento es del 15%")
+elif edad > 18 and estrato == 2:
+    descuento = 0.10  # 10%
+    print("Tu descuento es del 10%")
+elif edad <= 18 and estrato == 2:
+    descuento = 0.05  # 5%
+    print("Tu descuento es del 5%")
+else:
+    descuento = 0
+    print("No aplicas a descuento")
+
+precio_final = precio_matricula - (precio_matricula * descuento)
+
+print(f"El precio final de la matrícula después del descuento es: {precio_final}")
