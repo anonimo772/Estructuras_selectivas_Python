5e9899fe8be93721bcff8e9503c974a12c4663a6
unknown <Administrador@BOGDFPCGMF4137.FormacionSena.RED>
2025-03-19 09:24:03 -0500

    actividad_1}

---
+++ Actividad_1.py
@@ -0,0 +1,9 @@
+motor = float(input("Ingresa la temperatura del motor: ")) 
+print("Temperatura del motor:", motor)
+
+if motor > 80:
+    print("Motor apagado")
+else:
+    print("Motor encendido")
+
+print("Fin del programa")
